024c61e53045c1f83f14a5268a585199b5f1de69
Gian Cedrick G. Epilan <epilred@icloud.com>
2021-02-02 10:18:15 +0800

    barely barely working

--- test/test_3d_cube/test_3d_cube.cpp
+++ test/test_3d_cube/test_3d_cube.cpp
@@ -13,11 +13,15 @@ constexpr int m_height = 1080;
 constexpr int m_width = 720;
 
 test_3d_cube::test_3d_cube()
-	: m_Proj(glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.0f, 10.0f)),
+	: // m_Proj(
+	  // 	  glm::ortho(0.0f, (float)m_height, 0.0f, float(m_width),
+	  // -1.0f, 1.0f)),
+	  //   m_View(glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0))),
+	  m_Proj(glm::perspective(60.0f, (float)(m_height / m_width), 0.1f, 10.0f)),
 	  m_View(glm::lookAt(glm::vec3(1.2f, 1.2f, 1.2f),
 						 glm::vec3(0.0f, 0.0f, 0.0f),
-						 glm::vec3(0.0f, 0.0f, 1.0f))),
-	  m_TranslationA(200, 200, 0)
+						 glm::vec3(0.0f, 0.0f, 0.0f))),
+	  m_TranslationA(0, 0, 0)
 {
 	// float vertices[] = {
 	// 	1.0f,  1.0f,  1.f,	1.0f, 0.0f, 0.0f, // 0
@@ -47,10 +51,10 @@ test_3d_cube::test_3d_cube()
 
 	float vertices[] = {
 		//  vertex coords   texture coords
-		-50.0f, -50.0f, 0.0f, 0.0f, // top right    0
-		50.0f,	 -50.0f, 1.0f, 0.0f, // bottom right 1
-		50.0f,	 50.0f,  1.0f, 1.0f, // bottom left  2
-		-50.0f, 50.0f,  0.0f, 1.0f  // top left     3
+		-0.5f, -0.5f, 0.0f, 0.0f, // top right    0
+		0.5f,  -0.5f, 1.0f, 0.0f, // bottom right 1
+		0.5f,  0.5f,  1.0f, 1.0f, // bottom left  2
+		-0.5f, 0.5f,  0.0f, 1.0f  // top left     3
 	};
 
 	unsigned int indices[] = {
@@ -80,13 +84,15 @@ void test_3d_cube::OnUpdate(float deltaTime) {}
 void test_3d_cube::OnRender()
 {
 	GLCall(glClearColor(0.0f, 0.0f, 0.0f, 1.0f));
-	GLCall(glClear(GL_COLOR_BUFFER_BIT));
+	GLCall(glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT));
+	GLCall(glEnable(GL_DEPTH_TEST));
 
 	Renderer renderer;
 
 	m_Texture->Bind(0);
 
 	glm::mat4 model = glm::translate(glm::mat4(1.0f), m_TranslationA);
+
 	glm::mat4 mvp = m_Proj * m_View * model;
 	m_Shader->Bind();
 	m_Shader->SetUniformMat4f("u_MVP", mvp);
